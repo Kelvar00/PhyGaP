794f36bf5a2a517be76a085f2673a51348d1de44
Kelvar00 <joe20040226@126.com>
2026-03-23 13:22:01 +0800

    Configure GitHub Actions for web-source branch deployment fix2

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/PhyGaP',
+  base:'/PhyGaP/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
